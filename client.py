e49875e54fa851bbda04d3ce8a70087912eb4a22
José Erasmo Hernández Yate <joshdev35@gmail.com>
2026-06-26 12:45:04 -0600

    Testing the request between client and server

--- client.py
+++ client.py
@@ -7,5 +7,14 @@ client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.bind(CLIENTADDR)
 client.connect(SERVERADDR)
 
-msg = client.recv(1600).decode();
-print(msg)
+data=""
+while data!="N" or data!="n":
+    data = input("Digit a message to send to the server: ").encode()
+    print(f"[[Me]]: {data}")
+    if data != "N" or data!="n":
+        client.send(data)
+    else:
+        client.send("n".encode())
+        break
+    msg=client.recv(1600).decode()
+    print(f"[[Server]]: {msg}")
--- server.py
+++ server.py
@@ -4,7 +4,15 @@ server = socket.create_server(('localhost',80),family=socket.AF_INET)
 server.listen(1)
 conn,addr= server.accept()
 
-
-data="Hello from server!!!"
-conn.send(data.encode())
-conn.close()
+data=conn.recv(1600).decode().lower() 
+print(f"[[Client]]: {data}")
+while data != "n":
+    msg = input("Something to send: ").encode()
+    conn.send(msg)
+    print(f"[[Me]]: {msg}")
+    data=conn.recv(1600).decode().lower() 
+    if data != "n":
+        print(f"[[Client]]: {data}")
+    else:
+        conn.close()
+    
